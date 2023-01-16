c825af9dfe995ffe5235be9b94c31fc163bb0eab
cxl229 <cxl229@nyu.edu>
2023-01-16 15:05:46 -0500

    special styling for featured project tag

--- src/components/Project.js
+++ src/components/Project.js
@@ -9,11 +9,15 @@ function Project(props) {
         );
     });
 
+    // Show that a project is featured with a star tag
+    let tag = props.featured ? "featured ★" : "";
+
     return (
         <div className="project dropShadow">
             <img className="projectImage" src={props.image} alt={props.projectName + " image"}/>
-            <div>
+            <div className="projectText">
                 <h4>{props.projectName}</h4>
+                <h5 className="featuredTag">{tag}</h5>
                 <h5>{props.subtitle}</h5>
                 <h6>{props.techStack}</h6>
                 <p className="projectDescription">
