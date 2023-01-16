cdf83682ec9d915a44c492b7dcf29be5a8a6d855
cxl229 <cxl229@nyu.edu>
2023-01-16 18:37:12 -0500

    made about page bio easier to edit

--- src/components/About.js
+++ src/components/About.js
@@ -4,6 +4,9 @@ import './About.css';
 import portrait from "../img/portrait.jpg";
 
 function About() {
+    const bio = "I am a student at New York University (class of 2023) with a major in computer science and a minor in digital art & design. " +
+    "I'm passionate about applying creativity to computing in order to solve problems and develop unique programs. " +
+    "In my free time, I like to read, write, and do game development. Please check out some of my coding projects on the projects page.";
     return (
         <motion.div
             initial={{x: "50%"}}
@@ -17,9 +20,7 @@ function About() {
                 <div>
                     <h3 className="bio">Hi, I'm Charlotte Lin!</h3>
                     <p className="bio">
-                        I am a student at New York University (class of 2023) with a major in computer science and a minor in digital art & design.
-                        I'm passionate about applying creativity to computing in order to solve problems and develop unique programs.
-                        In my free time, I like to read, write, and play video games. Please check out some of my coding projects on the projects page.
+                        {bio}
                         <br/><br/>
                         If you'd like to contact me, please email me at <a className="emailAddress" href="mailto:charlottexlin02@gmail.com">charlottexlin02@gmail.com</a>.
                     </p>
